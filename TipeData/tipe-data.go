c3dadee2ab9b5f922e62b12e2ee62608bedb060f
Abdurrahman <Abdrhmn.gh@gmail.com>
2024-09-04 21:21:30 +0700

    initial commit

---
+++ TipeData/tipe-data.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// tipe data number:
+	// integer (bilangan bulat) -> int8, int16, int32, int64
+	// integer (khusus bilangan positif 0 - sekian) -> uint8, uint16, uint32, uint64
+
+	// tipe data floating point:
+	// float32, float64, complex64, complex128
+
+	// alias name untuk tipe data integer dan floating point :
+	// byte -> int8
+	// rune -> int32
+	// int -> minimal int32 ke atas
+	// uint -> minimal uint32 ke atas
+
+	fmt.Println("Hasil: ", 1)
+	fmt.Println("Hasil 2: ", 10.5)
+}
